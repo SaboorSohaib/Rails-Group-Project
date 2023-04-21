5c45630e375238f1432cef09ede0fd1e26d6a007
mule <belmeetmule@gmail.com>
2023-04-22 02:15:01 +0300

    Improvement: updated recipe-foods and recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,6 +7,7 @@ class RecipesController < ApplicationController
     @user = User.find(params[:user_id])
     @recipe = Recipe.find(params[:id])
     @foods = Food.all
+    @recipe_foods = @recipe.recipe_foods.includes(:food, :recipe).order(:id)
   end
 
   def new
